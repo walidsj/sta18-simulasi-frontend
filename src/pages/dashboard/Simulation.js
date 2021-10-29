bfae615276c3f7d0d56e5a6079fe37a14996f77b
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-10-29 16:57:58 +0700

    Update Simulation.js

--- src/pages/dashboard/Simulation.js
+++ src/pages/dashboard/Simulation.js
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import { useRecoilValue } from 'recoil';
 import { userState } from '../../stores/user';
 import { Heading } from '@chakra-ui/layout';
+import { Alert, AlertIcon } from '@chakra-ui/alert';
 
 const Simulation = () => {
   const user = useRecoilValue(userState);
@@ -13,6 +14,14 @@ const Simulation = () => {
       <Heading size="lg" mb="4" fontWeight="bold">
         Simulasi
       </Heading>
+      <Alert status="warning" alignItems="start">
+        <AlertIcon />
+        Perlu diingat bahwa proses dan hasil dari simulasi ini bersifat
+        probable, sehingga kondisi yang tergambarkan dari hasil simulasi ini
+        tidak 100% benar namun tetap bisa membantu dalam pengambilan keputusan
+        ketika pengisian Survei/Polling Penempatan oleh Biro SDM Kementerian
+        Keuangan.
+      </Alert>
     </DashboardLayout>
   );
 };
