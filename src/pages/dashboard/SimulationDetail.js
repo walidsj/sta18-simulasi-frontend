b2476640b96409d300e416917fa588609d327329
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-10-29 18:01:46 +0700

    Update SimulationDetail.js

--- src/pages/dashboard/SimulationDetail.js
+++ src/pages/dashboard/SimulationDetail.js
@@ -8,6 +8,9 @@ import { Spinner } from '@chakra-ui/spinner';
 import { useParams } from 'react-router-dom';
 import BackButton from '../../components/ui/BackButton';
 import trialService from '../../services/trialService';
+import { Alert, AlertIcon } from '@chakra-ui/alert';
+import dayjs from 'dayjs';
+import Countdown from 'react-countdown';
 
 const SimulationDetail = () => {
   const user = useRecoilValue(userState);
@@ -16,30 +19,81 @@ const SimulationDetail = () => {
   const [trial, setTrial] = useState();
   const [isLoading, setIsLoading] = useState();
 
+  const fetchTrial = async () => {
+    setIsLoading(true);
+    const { data } = await trialService.get(id, user.token);
+    setTrial(data);
+    console.log(data);
+    setIsLoading(false);
+  };
+
   // fetching trial
   useEffect(() => {
-    const fetchTrial = async () => {
-      setIsLoading(true);
-      const { data } = await trialService.get(id, user.token);
-      setTrial(data);
-      console.log(data);
-      setIsLoading(false);
-    };
-
     fetchTrial();
   }, [user, setIsLoading]);
 
+  // simulasi tidak ditemukan
   if (!trial)
     return (
-      <DashboardLayout>
-        <Helmet title="Simulasi" />
-        <Heading size="lg" mb="4" fontWeight="bold">
-          Simulasi
-        </Heading>
+      <Layout trial={{ title: 'Simulasi' }}>
         {isLoading ? <Spinner /> : <Text>Simulasi tidak ditemukan.</Text>}
-      </DashboardLayout>
+      </Layout>
     );
 
+  // simulasi sudah buka dan belum tutup
+  if (
+    dayjs(trial.opened_at) < dayjs(new Date()) &&
+    dayjs(trial.closed_at) > dayjs(new Date())
+  )
+    return (
+      <Layout trial={trial}>
+        {trial.trial_options.length > 0 ? (
+          trial.trial_options.map(({ title }) => <Text>{title}</Text>)
+        ) : (
+          <Alert status="warning" p="0" variant="subtle" rounded="full">
+            <AlertIcon />
+            <Text as="div" fontSize="sm">
+              Belum ada opsi pilihan tersedia
+            </Text>
+          </Alert>
+        )}
+      </Layout>
+    );
+
+  // simulasi sudah tutup
+  if (dayjs(trial.closed_at) < dayjs(new Date()))
+    return (
+      <Layout trial={trial}>
+        <Alert status="error" p="0" variant="subtle" rounded="full">
+          <AlertIcon />
+          <Text as="div" fontSize="sm">
+            Simulasi sudah ditutup.
+          </Text>
+        </Alert>
+      </Layout>
+    );
+
+  // simulasi belum buka
+  return (
+    <Layout trial={trial}>
+      <Alert status="success" p="0" variant="subtle" rounded="full">
+        <AlertIcon />
+        <Text as="div" fontSize="sm">
+          Simulasi belum buka. Dibuka dalam{' '}
+          <Text
+            as={Countdown}
+            fontWeight="bold"
+            date={dayjs(trial.opened_at)}
+            zeroPadDays={0}
+            onComplete={() => fetchTrial()}
+          />
+        </Text>
+      </Alert>
+    </Layout>
+  );
+};
+
+const Layout = ({ trial, children }) => {
   return (
     <DashboardLayout>
       <Helmet title={trial.title} />
@@ -48,6 +102,7 @@ const SimulationDetail = () => {
         {trial.title}
       </Heading>
       <Text mb="6">{trial.description}</Text>
+      {children}
     </DashboardLayout>
   );
 };
